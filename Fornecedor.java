d32d3ede31c021eb54d46adb7316db58105947e0
Lucas Silva <luckassilvallima@hotmail.com>
2021-12-11 17:30:56 -0300

    Lambdas em JAVA

---
+++ Fornecedor.java
@@ -0,0 +1,14 @@
+package lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+		
+		Supplier<List<String>> umList = () -> Arrays.asList("Ana", "Bia", "Gui");
+		System.out.println(umList.get());
+	}
+}
